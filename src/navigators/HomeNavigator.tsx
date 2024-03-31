93a5f02c0ae59d56898501e5ec37a7f8cd44f689
serhatakhan <serhat.akhan@outlook.com>
2024-03-31 17:13:12 +0300

    cart screen added & header designed

--- src/navigators/HomeNavigator.tsx
+++ src/navigators/HomeNavigator.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/HomeScreen";
-import { Image, Text, TouchableOpacity } from "react-native";
+import { Dimensions, Image, Text, TouchableOpacity, View } from "react-native";
 import CategoryFilterScreen from "../screens/CategoryFilterScreen";
 import ProductDetailsScreen from "../screens/ProductDetailsScreen";
 import { Ionicons } from '@expo/vector-icons';
 import { getFocusedRouteNameFromRoute, useNavigation } from "@react-navigation/native";
 import { Foundation } from '@expo/vector-icons';
+import CartScreen from "../screens/CartScreen"
+
+const {width, height} = Dimensions.get("window")
 
 const Stack = createStackNavigator();
 
@@ -58,6 +61,14 @@ function MyStack( {navigation, route} ) {
           headerTitle: () => (
             <Text style={{fontWeight: "bold", fontSize: 17, color: "white"}}>Ürünler</Text>
           ),
+          headerRight: ()=> (
+            <TouchableOpacity onPress={()=> navigation.navigate("CartScreen")} style={{width: width*0.22, height: 33, backgroundColor: "white", marginRight: width*0.03, borderRadius: 10, flexDirection: "row", alignItems: "center"}}>
+              <Image source={require("../../assets/cart.png")} style={{width: 24, height: 24, marginLeft: 5, marginRight: 3}} />
+              <View style={{flex: 1, justifyContent: "center", alignItems: "center", height: 33, backgroundColor: "#f3effe", borderTopRightRadius: 10, borderBottomRightRadius: 10}}>
+                <Text style={{color: "#5d3ebd", fontWeight: "bold", fontSize: 12}}>₺24,00</Text>
+              </View>
+            </TouchableOpacity>
+          )
         }}
       />
       <Stack.Screen
@@ -71,12 +82,12 @@ function MyStack( {navigation, route} ) {
           // header'ın soluna x koyacağız
           headerLeft: ()=> (
             <TouchableOpacity onPress={()=> navigation.goBack()} style={{paddingLeft: 10}}>
-              <Ionicons name="close" size={25} color="white" />
+              <Ionicons name="close" size={26} color="white" />
             </TouchableOpacity>
           ),
           headerRight: ()=> (
             <TouchableOpacity style={{paddingRight: 10}}>
-              <Foundation name="heart" size={25} color="#32177a" />
+              <Foundation name="heart" size={26} color="#32177a" />
             </TouchableOpacity>
           ),
           headerStyle: {
@@ -86,6 +97,30 @@ function MyStack( {navigation, route} ) {
             <Text style={{fontWeight: "bold", fontSize: 17, color: "white"}}>Ürün Detayı</Text>
           ),
         }}
+      />
+      <Stack.Screen
+      name="CartScreen"
+      component={CartScreen}
+      options={{
+        headerBackTitleVisible: false,
+        headerTintColor: "white",
+        headerStyle: {
+          backgroundColor: "#5c3ebc",
+        },
+        headerTitle: ()=> (
+          <Text style={{fontWeight: "bold", fontSize: 16, color: "white"}}>Sepetim</Text>
+        ),
+        headerLeft: ()=> (
+          <TouchableOpacity onPress={()=> navigation.goBack()} style={{paddingLeft: 10}} >
+            <Ionicons name="close" size={26} color="white" />
+          </TouchableOpacity>
+        ),
+        headerRight: ()=> (
+          <TouchableOpacity style={{paddingRight: 10}} >
+            <Ionicons name="trash" size={25} color="white" />
+          </TouchableOpacity>
+        ),
+      }}
       />
     </Stack.Navigator>
   );
