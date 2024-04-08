73b4f905e7881b721061b32e17836946ba3f0d76
serhatakhan <serhat.akhan@outlook.com>
2024-04-08 17:51:10 +0300

    store has been installed

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import 'react-native-gesture-handler';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import RootNavigator from './src/navigators/RootNavigator';
+import "react-native-gesture-handler";
+import { LogBox, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import RootNavigator from "./src/navigators/RootNavigator";
+import store from "./src/redux/store";
+import { Provider } from "react-redux";
+
+// LogBox'u import edip bu şekilde kullanınca uyarılar simülatöre yansımıyor
+LogBox.ignoreAllLogs(); // Ignore all log notifications
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     // marginTop: "15%"
   },
 });
